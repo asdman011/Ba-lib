028193bfd2fda1155e97dd9acc9cd8bde4b93cfd
Ammar Ashour <am9370337@gmail.com>
2024-10-20 23:44:37 +0300

    Refactor import statement in urls.py

--- baġlib_project/baġlib_project/urls.py
+++ baġlib_project/baġlib_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import index
+from core.views import index
 
 urlpatterns = [
     path('', index, name='index'),
