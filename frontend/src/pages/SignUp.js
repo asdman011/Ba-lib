f2050d445ee725e16e9c0e31a0eef56c48e9cdaa
Ammar Ashour <am9370337@gmail.com>
2024-10-20 23:14:35 +0300

    Add Sign-up route and component

---
+++ frontend/src/pages/SignUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <input type="text" placeholder="Username" required />
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
