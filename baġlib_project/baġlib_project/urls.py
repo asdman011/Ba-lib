b8fabc810c3e901886d22cb38cfc45c16c55ad4d
Ammar Ashour <am9370337@gmail.com>
2024-10-21 23:22:59 +0300

    Refactor URLs in settings.py
    - Provide all the necessary URLs for login, logout, signup, and OAuth-based login (e.g., /accounts/login/, /accounts/signup/, /accounts/google/login/)

--- baġlib_project/baġlib_project/urls.py
+++ baġlib_project/baġlib_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
@@ -26,6 +26,8 @@ urlpatterns = [
     path('signup/', index),
     path('login/', index, name='login'),
     path('dashboard/', index, name='dashboard'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),  # Include allauth URLs for OAuth
     # Add more paths if necessary
 ]
 
