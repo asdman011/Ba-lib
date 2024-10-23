3d1c1b62ec56868ce02f168dcc2d0eb316028448
Ammar Ashour <am9370337@gmail.com>
2024-10-24 00:50:23 +0300

    Register Book, ReadingProgress, and Folder models in admin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book, ReadingProgress, Folder
 
-# Register your models here.
+# Register your models here
+admin.site.register(Book)
+admin.site.register(ReadingProgress)
+admin.site.register(Folder)
