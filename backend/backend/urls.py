c91c26cc2f3f46583cb22a237b90506918e2bc22
Ammar Ashour <am9370337@gmail.com>
2024-10-24 00:41:45 +0300

    Configure media file handling for profile pictures

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # your other routes
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
