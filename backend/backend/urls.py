a0abbc969dd6af436fd79ebe0284576c5303dc5a
Ammar Ashour <am9370337@gmail.com>
2024-10-24 00:56:29 +0300

    Remove unused index view function

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,10 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')
 
 urlpatterns = [
     # your other routes
