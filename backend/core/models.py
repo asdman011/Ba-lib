6ce7149138b3644f189717f852c076ccd6479a9a
Ammar Ashour <am9370337@gmail.com>
2024-10-24 00:21:21 +0300

    Add User, Book, ReadingProgress, and Folder models

--- backend/core/models.py
+++ backend/core/models.py
@@ -4,3 +4,30 @@ from django.db import models
 class User(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    total_pages = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class ReadingProgress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    current_page = models.IntegerField(default=0)
+    last_read_date = models.DateTimeField(auto_now=True)
+    streak_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
+
+class Folder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.name}"
