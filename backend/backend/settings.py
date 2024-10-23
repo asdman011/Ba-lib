1fde76e227f21793fdbb4cf780f565793fcf590b
Ammar Ashour <am9370337@gmail.com>
2024-10-24 00:19:57 +0300

    Initialize ORM

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r1z$xk6lj74vdpj#*!$7q#0&w&ni807-)ig@dk6m9kr(w6&mt7'
+SECRET_KEY = 'django-insecure-2onz97%488n7jprof7c1kndf0dl#9(-!f8-0ix!&(r$w+qt)u+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
