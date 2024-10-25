e6617f562c8972070ba263725746b1359a854774
Ammar Ashour <am9370337@gmail.com>
2024-10-25 21:44:28 +0300

    Refactor static file paths in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('dashboard/', index, name='dashboard'),
     path('signup/', index),   
     # Add more paths if necessary
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
