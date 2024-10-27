58ef6c53f497457a40ee90f6f7937192a02968b6
Ammar Ashour <am9370337@gmail.com>
2024-10-27 21:52:19 +0300

    Refactor STATICFILES_DIRS paths in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,13 +14,13 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join('/mnt/d/Cources/Projects/Baġlib/repo/Baglib/frontend/build/static'),  # Adjust the path to your React build directory
-    os.path.join('/mnt/d/Cources/Projects/Baġlib/repo/Baglib/frontend/build'), # Add the path to your frontend build directory (to include manifest.json)  
+    os.path.join(BASE_DIR, 'frontend/build/static'),  # Adjust the path to your React build directory
+    os.path.join(BASE_DIR, 'frontend/build'),  # Add the path to your frontend build directory (to include manifest.json)
 ]
 # Directory where collectstatic will collect static files for production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -145,7 +145,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
